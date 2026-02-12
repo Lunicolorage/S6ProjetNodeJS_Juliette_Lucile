8d8c07c140af922942f85f022416bfafbd79b385
KvmjfuufEvdspt <juliette.ducros2005@gmail.com>
2026-02-12 15:41:24 +0100

    début CreateEvent

--- front/src/components/CreateEvent.jsx
+++ front/src/components/CreateEvent.jsx
@@ -2,7 +2,22 @@ function CreateEvent(){
 
     return(
         <div>
-            <h1>Création d'un évènement</h1>
+            <button></button>
+            <div>
+                <h1>Création d'un évènement</h1>
+
+                <label htmlFor="titreEvent">Titre de l'évènement : </label>
+                <input id="titreEvent" type="text"></input>
+
+                <label htmlFor="descriptionEvent">Description : </label>
+                <textarea id="descriptionEvent" rows="4"></textarea>
+
+                <label htmlFor="dateEvent">Date : </label>
+                <input id="dateEvent" type="date"></input>
+
+                <label htmlFor="lieuEvent">Lieu : </label>
+                <input id="lieuEvent" type="text"></input>
+            </div>
         </div>
     )
 }
